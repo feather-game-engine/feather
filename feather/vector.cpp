f76aa5a63e837d6b27d3fb1a0eb4a528dc895160
A K <kadamlita@gmail.com>
2022-09-28 20:38:01 +0200

    vectors WIP

---
+++ feather/vector.cpp
@@ -0,0 +1,7 @@
+#include "vector.h"
+
+void Vector::Normalize(){
+	//Get the magnitude of the vector w/pythagoras.
+	//Divide the magnitude by itself
+	//Set the vector's x and y to this new magnitude
+}
---
+++ feather/vector.h
@@ -0,0 +1,11 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector{
+	public:
+		int x;
+		int y;
+	
+	void Normalize();
+
+#endif
